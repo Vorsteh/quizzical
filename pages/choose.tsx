f352876c435f56084357f667eba28ded377a0edd
Emil <Whyzoid@gmail.com>
2023-03-23 14:37:11 +0100

    Change mt to pt
    Change mt to pt so that page is not taller than 100vh

--- pages/choose.tsx
+++ pages/choose.tsx
@@ -5,7 +5,7 @@ const Choose = () => {
     const [info, setInfo] = useState({})
     return (
         <div>
-            <h1 className="mt-10 text-3xl font-semibold text-center text-gray-100">Welcome to Quizzical!</h1>
+            <h1 className="pt-10 text-3xl font-semibold text-center text-gray-100">Welcome to Quizzical!</h1>
         </div>
     )
 }
