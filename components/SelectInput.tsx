662b88b84bc363a3e9cc54c0fdae6853f1f06cc8
Emil <Whyzoid@gmail.com>
2023-03-24 13:28:02 +0100

    Select Input
    Fixed and started working on the Select Input component.

--- components/SelectInput.tsx
+++ components/SelectInput.tsx
@@ -1,15 +1,22 @@
 interface SelectProps {
     text: string;
-    type: any;
     value: any;
     options: string[];
     name: string;
     onChange: any;
 }
-const SelectInput: React.FC<SelectProps> = () => {
-    return (
-        <div>
+const SelectInput: React.FC<SelectProps> = ({text, value, options, name, onChange}) => {
 
+    const optionList = options.map(option => {
+        return <option value={option} className='bg-gray-100'>{option}</option>
+    })
+
+    return (
+        <div className='relative pb-5'>
+            <p className='font-bold'>{text}</p>
+            <select name={name} className='p-2 outline-none w-full rounded-sm bg-gray-300'>
+                {optionList}
+            </select>
         </div>
     )
 }
--- pages/choose.tsx
+++ pages/choose.tsx
@@ -1,9 +1,12 @@
 import QuizInput from "@/components/QuizInput";
+import SelectInput from "@/components/SelectInput";
 import React from "react";
 import { useState } from "react";
 const Choose = () => {
 
-    const [info, setInfo] = useState({amount: 5, category: 'Science: Mathematics', difficulty: 'Medium', type: 'Multiple Choice'})
+    const [info, setInfo] = useState({amount: 5, category: 'Select category', difficulty: 'Medium', type: 'Multiple Choice'})
+
+    const categorys: string[] = ['-Select category-','General knowledge', 'Science: Computers','Science: Mathematics']
 
     const handleChange = (e: any) => {
         setInfo(prevInfo => {
@@ -16,14 +19,19 @@ const Choose = () => {
     return (
         <div className="relative flex justify-center w-full h-full lg:bg-[url('/images/choose.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
             <div className="w-full h-full p-16 backdrop-blur-sm">
-                <div className="font-semibold text-center text-gray-800">
-                    <h1 className="text-6xl ">Quizzical</h1>
-                    <h2 className="text-2xl">You can create the type of trivia you want here!</h2>
+                <div className="w-fit m-auto h-auto bg-black bg-opacity-40 rounded-3xl p-14">
+                    <div className="font-semibold text-center text-gray-800">
+                        <h1 className="text-6xl">Quizzical</h1>
+                        <h2 className="text-2xl">You can create the type of trivia you want here!</h2>
+                    </div>
+                    <form className="flex flex-col self-center w-1/3 m-auto mt-10">
+                        <QuizInput text="Number of Questions:" value={info.amount} onChange={handleChange} name="amount" type="text"/>
+                        <SelectInput name="category" options={categorys} text='Select category' value={info.category} onChange={handleChange} />
+                        <SelectInput name="difficulty" options={['Any','Easy', 'Medium', 'Hard']} text='Select difficulty' value={info.difficulty} onChange={handleChange} />
+                        <SelectInput name="type" options={['Any type', 'True / False', 'Multiple Choice']} text='Select type' value={info.type} onChange={handleChange} />
+                    
+                    </form>
                 </div>
-                <form className="flex flex-col self-center w-1/3 m-auto mt-10">
-                    <QuizInput text="Number of Questions:" value={info.amount} onChange={handleChange} name="amount" type="text"/>
-                    <QuizInput text="Select Category:" value={info.category} onChange={handleChange} name="category" type="select"/>
-                </form>
             </div>
         </div>
     )
