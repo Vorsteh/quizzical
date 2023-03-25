b71124ce62658e5b9b6b6f9b756eeb97b3788fe7
Emil <Whyzoid@gmail.com>
2023-03-26 00:13:42 +0100

    Tried to load all categories
    Tried to save all categories in an array of object and render all of them in the dropdown. Ended up with a bunch of errors.

--- components/SelectInput.tsx
+++ components/SelectInput.tsx
@@ -1,20 +1,20 @@
 interface SelectProps {
     text: string;
     value: any;
-    options: string[];
+    options: any[];
     name: string;
     onChange: any;
 }
 const SelectInput: React.FC<SelectProps> = ({text, value, options, name, onChange}) => {
 
     const optionList = options.map(option => {
-        return <option value={option} key={option} className='bg-gray-100'>{option}</option>
+        return <option value={0} key={option} className='bg-gray-100'>{option}</option>
     })
 
     return (
         <div className='relative pb-5'>
             <p className='font-bold'>{text}</p>
-            <select name={name} className='w-full p-2 bg-gray-300 rounded-sm outline-none'>
+            <select name={name} className='w-full p-2 bg-gray-300 rounded-sm outline-none' onChange={onChange}>
                 {optionList}
             </select>
         </div>
--- pages/choose.tsx
+++ pages/choose.tsx
@@ -2,11 +2,10 @@ import QuizInput from "@/components/QuizInput";
 import SelectInput from "@/components/SelectInput";
 import React from "react";
 import { useState } from "react";
+import { categories } from "@/components/categories";
 const Choose = () => {
 
     const [info, setInfo] = useState({amount: 5, category: 'Select category', difficulty: 'Medium', type: 'Multiple Choice'})
-
-    const categorys: string[] = ['-Select category-','General knowledge', 'Science: Computers','Science: Mathematics']
 
     const handleChange = (e: any) => {
         setInfo(prevInfo => {
@@ -15,20 +14,21 @@ const Choose = () => {
                 [e.target.name]: e.target.value
             }
         })
+        console.log(info)
     }
     return (
         <div className="relative flex justify-center w-full h-full lg:bg-[url('/images/choose.jpg')] bg-no-repeat bg-center bg-fixed bg-cover">
             <div className="w-full h-full p-16 backdrop-blur-sm">
-                <div className="w-fit m-auto h-auto bg-black bg-opacity-40 rounded-3xl p-14">
+                <div className="h-auto m-auto bg-black w-fit bg-opacity-40 rounded-3xl p-14">
                     <div className="font-semibold text-center text-gray-800">
                         <h1 className="text-6xl">Quizzical</h1>
                         <h2 className="text-2xl">You can create the type of trivia you want here!</h2>
                     </div>
                     <form className="flex flex-col self-center w-1/3 m-auto mt-10">
                         <QuizInput text="Number of Questions:" value={info.amount} onChange={handleChange} name="amount" type="text"/>
-                        <SelectInput name="category" options={categorys} text='Select category' value={info.category} onChange={handleChange} />
+                        <SelectInput name="category" options={categories} text='Select category' value={info.category} onChange={handleChange} />
                         <SelectInput name="difficulty" options={['Any','Easy', 'Medium', 'Hard']} text='Select difficulty' value={info.difficulty} onChange={handleChange} />
-                        <SelectInput name="type" options={['Any type', 'True / False', 'Multiple Choice']} text='Select type' value={info.type} onChange={handleChange} />
+                        <SelectInput name="type" options={[{'Any type':'any'}, {'True / False': 'boolean'}, {'Multiple Choice':'multiple'}]} text='Select type' value={info.type} onChange={handleChange} />
                     
                     </form>
                 </div>
